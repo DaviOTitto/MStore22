7e1dcf976879c0aade47fe1155edc19d68243e59
Davi <davioliveiratio@gmail.com>
2024-04-30 16:52:32 -0300

    mensagem

--- novo_site/views/dados.py
+++ novo_site/views/dados.py
@@ -23,6 +23,7 @@ from matplotlib import pyplot as plt
 from openpyxl import load_workbook
 from openpyxl_image_loader import SheetImageLoader
 import PIL.Image as Image
+import MySQLdb
 
 from ..models import *
 from ..forms import *
@@ -31,6 +32,34 @@ from ..forms import *
 
 home = TemplateView.as_view(template_name='pag2.html')
 emprodu = TemplateView.as_view(template_name='emprodu.html')
+def  conectaMysql():
+    con = MySQLdb.connect.connect(host='186.202.152.195',
+                           database='cecotein3',
+                           user='cecotein3',
+                           password='@vsy8y3',
+                           port=3306)
+    return con
+
+
+def adciona_automatico(request):
+        
+    con_mysql = conectaMysql()
+    cur_mysql = con_mysql.cursor()
+    data = datetime.today().strftime("%d/%m/%Y %H:%M:%S")
+
+            #inserção de dados por backend
+    sql = '''INSERT into Pedido 
+            ("codped_ped","cnpj","hora_ped")
+                    values(%s,%s,%s)'''
+    sql2 = '''INSERT into ItemPed 
+            ("coditem_ite","Código_da_empresa","Código_do_produto","Quantidade","Preço")
+            values(%s,%s,%s,%s)'''            
+    lista_insert = [auto,"51738180697",data]                      
+    lista_insert = [auto2,3,19634,1,100.12]   
+
+
+
+
 def completa_lista(request):
     
     if request.method == 'POST':  
